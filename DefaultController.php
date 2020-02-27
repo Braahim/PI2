f23acef23388165497fa4c52dcc373dbd51f8d46
wafaaaaaa <44993839+wafaaaaaa@users.noreply.github.com>
2020-02-27 09:28:49 +0100

    Add files via upload

--- DefaultController.php
+++ DefaultController.php
@@ -1,13 +1,21 @@
 <?php
 
-namespace StockBundle\Controller;
+namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
     {
-        return $this->render('StockBundle:Default:index.html.twig');
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
     }
 }
